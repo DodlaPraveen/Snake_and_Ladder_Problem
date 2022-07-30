e479ec77feedc0eee3b9a66e4d73bb7ea6ab5820
praveen dodla <dodlapraveen09@gmail.com>
2022-07-30 17:21:33 +0530

    Singlr Player Started o

---
+++ Snake_Lader/Snake_Lader/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_Lader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Simulation(Single Player)");
+            int playerOnePosition = 0;
+            Console.WriteLine("Position of Player 1 is at " + playerOnePosition);
+        }
+    }
+}
