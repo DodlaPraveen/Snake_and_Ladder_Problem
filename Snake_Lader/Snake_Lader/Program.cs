6058ee37d9dbecb4c9ce6011c35bbe07f9b8929d
praveen dodla <dodlapraveen09@gmail.com>
2022-07-30 17:47:24 +0530

    Repate The Die Postion 100

--- Snake_Lader/Snake_Lader/Program.cs
+++ Snake_Lader/Snake_Lader/Program.cs
@@ -13,23 +13,26 @@ namespace Snake_Lader
             int playerOnePosition = 0;
             Random random = new Random();
             int dieRoll;
-            dieRoll = random.Next(1, 7);
-            int check = random.Next(1, 4);
-            switch (check)
+            while (playerOnePosition < 100)
             {
-                case NO_PLAY:
-                    playerOnePosition = playerOnePosition + 0;
-                    break;
-                case LADDER:
-                    playerOnePosition = playerOnePosition + dieRoll;
-                    break;
-                case SNAKE:
-                    playerOnePosition = playerOnePosition - dieRoll;
-                    if (playerOnePosition < 0)
-                        playerOnePosition = 0;
-                    break;
+                dieRoll = random.Next(1, 7);
+                int check = random.Next(1, 4);
+                switch (check)
+                {
+                    case NO_PLAY:
+                        playerOnePosition += 0;
+                        break;
+                    case LADDER:
+                        playerOnePosition += dieRoll;
+                        break;
+                    case SNAKE:
+                        playerOnePosition -= dieRoll;
+                        if (playerOnePosition < 0)
+                            playerOnePosition = 0;
+                        break;
+                }
+                Console.WriteLine("Position of Player 1 is at " + playerOnePosition);
             }
-            Console.WriteLine("Position of Player 1 is at " + playerOnePosition);
         }
     }
 }
