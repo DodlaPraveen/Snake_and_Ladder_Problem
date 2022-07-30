8cc45157ba989a2838b1dc68e5ef731872b20f29
praveen dodla <dodlapraveen09@gmail.com>
2022-07-30 17:30:33 +0530

    Player Rolles The Die

--- Snake_Lader/Snake_Lader/Program.cs
+++ Snake_Lader/Snake_Lader/Program.cs
@@ -8,6 +8,10 @@ namespace Snake_Lader
         {
             Console.WriteLine("Welcome to Snake and Ladder Simulation(Single Player)");
             int playerOnePosition = 0;
+            Random random = new Random();
+            int dieRoll;
+            dieRoll = random.Next(1, 7);
+            playerOnePosition += dieRoll;
             Console.WriteLine("Position of Player 1 is at " + playerOnePosition);
         }
     }
